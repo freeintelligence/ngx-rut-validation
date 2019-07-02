ea5dc24b345cfd6e6695d558b3978db300486ede
freeintelligence <free04381@gmail.com>
2019-07-02 16:50:27 -0400

    Se mejora la directiva 'format' para que funcione con los formularios.

--- projects/ngx-rut-validation/src/lib/directives/format.directive.ts
+++ projects/ngx-rut-validation/src/lib/directives/format.directive.ts
@@ -1,10 +1,14 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, ContentChild } from '@angular/core';
 import { format } from 'rut.js';
+import { NG_VALIDATORS, NgModel, FormControlName } from '@angular/forms';
 
 @Directive({
-  selector: '[rutFormat]'
+  selector: '[rutFormat]',
 })
 export class FormatDirective {
+
+  @ContentChild(NgModel, { static: true }) protected model: NgModel;
+  @ContentChild(FormControlName, { static: true }) protected formControlName: FormControlName;
 
   private element: HTMLInputElement;
 
@@ -16,6 +20,13 @@ export class FormatDirective {
   onKeyUp() {
     if (typeof this.element.value === 'string' && this.element.value.length > 1) {
       this.element.value = format(this.element.value);
+
+      if (this.model && this.model.update && this.model.update.emit) {
+        this.model.update.emit(this.element.value);
+      }
+      if (this.formControlName && this.formControlName.control && this.formControlName.control.setValue) {
+        this.formControlName.control.setValue(this.element.value);
+      }
     }
   }
 
