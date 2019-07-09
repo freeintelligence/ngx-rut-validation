1952d9b9ce09d43b2ff87142566bbf98b65aadab
freeintelligence <free04381@gmail.com>
2019-07-08 22:21:27 -0400

    Nuevas exportaciones de las directivas y pipes. Esto deberia haber estado desde la primer version, ni idea de como se me paso.

--- projects/ngx-rut-validation/src/public-api.ts
+++ projects/ngx-rut-validation/src/public-api.ts
@@ -3,4 +3,10 @@
  */
 
 export * from './lib/validators/index';
+
+export * from './lib/directives/default.directive';
+export * from './lib/directives/format.directive';
+
+export * from './lib/pipes/format.pipe';
+
 export * from './lib/ngx-rut-validation.module';
